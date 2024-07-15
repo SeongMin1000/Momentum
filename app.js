83d3d25369b39a7709366099a113e1725b1007f6
SeongMin1000 <russoll3579@gmail.com>
2024-07-16 00:58:40 +0900

    유저 정보 저장상태에 따른 레이아웃설정

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input")
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){ // 브라우저 동작에 관한 정보 기록 인자 객체
     event.preventDefault() // form 제출 시 새로고침 방지(브라우저 기본 동작)
     loginForm.classList.add(HIDDEN_CLASSNAME) // 제출 뒤 form 숨기기
     const username = loginInput.value
-    localStorage.setItem("username", username)
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME) // h1 나타나기
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreeting(username) // h1 나타나기
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreeting(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY)
+
+if(savedUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginSubmit)
+}
+else{
+    paintGreeting(savedUserName)
+}
