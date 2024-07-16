8d069faa97eea71e726a5f6520d0a465870a3c22
SeongMin1000 <russoll3579@gmail.com>
2024-07-16 22:03:29 +0900

    랜덤 배경 추가

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg"
+]
+
+const chooseImage = images[Math.floor(Math.random() * images.length)]
+
+const bgImage = document.createElement("img")
+bgImage.src = `img/${chooseImage}`
+document.body.appendChild(bgImage)
