8b98e813f80bcd3c066527b85a4af1818c4d0a8c
SeongMin1000 <russoll3579@gmail.com>
2024-07-17 21:24:03 +0900

    새로고침 정보 기억

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,11 @@ const toDoForm = document.getElementById("todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.getElementById("todo-list")
 
-const toDos = []
+const TODOS_KEY = "todos"
+let toDos = []
 
-function saveToDos(){
-    localStorage.setItem("todos",JSON.stringify(toDos)) // 객체나 배열을 json 문자열로 변환
+function saveToDo(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos)) // 객체나 배열을 json 문자열로 변환
 }
 
 function deleteToDo(event){
@@ -31,7 +32,15 @@ function handleToDoSubmit(event){
     toDoInput.value = "" // 엔터 누르면 입력값 삭제
     toDos.push(newTodo)
     paintToDo(newTodo)
-    saveToDos()
+    saveToDo()
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+const saveToDos = localStorage.getItem(TODOS_KEY)
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(saveToDos) // json 문자열을 기본 문자열로 파싱
+    toDos = parsedToDos
+    parsedToDos.forEach(paintToDo);
+}
+
