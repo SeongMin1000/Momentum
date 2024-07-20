e5d7a9069938e146ab07429e005dff9ed3e39469
SeongMin1000 <russoll3579@gmail.com>
2024-07-21 05:36:21 +0900

    UI 개선

--- js/background.js
+++ js/background.js
@@ -3,11 +3,12 @@ const images = [
     "1.jpg",
     "2.jpg",
     "3.jpg",
-    "4.jpg"
+    "4.jpg",
 ]
 
 const chooseImage = images[Math.floor(Math.random() * images.length)]
 
 const bgImage = document.createElement("img")
 bgImage.src = `img/${chooseImage}`
+bgImage.id = "background-image";
 document.body.prepend(bgImage)
--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,23 @@
-import { API_KEY } from "./api_key.js";
+import { API_KEY } from "./api_key.js"
 
 function onGeoOk(position){
     const lat = position.coords.latitude 
     const lng = position.coords.longitude
-    console.log("You live in", lat, lng)
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const weather = document.querySelector("#weather span:first-child")
-            const city = document.querySelector("#weather span:last-child")
+            const weather = document.querySelector("#weather-text span:first-child")
+            const city = document.querySelector("#weather-text span:last-child")
+            const weatherIcon = data.weather[0].icon;
+            const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
             city.innerText = data.name
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+            weather.innerText = `${data.main.temp}°`
+            const addIcon = document.createElement('img')
+            addIcon.src = weatherIconAdrs
+            addIcon.id = "weather-icon"
+            document.querySelector("#weather").prepend(addIcon)
     })
 }
 
